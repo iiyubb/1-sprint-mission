660ea07328793b856ca851503d5cc9eb249a4547
yubin8773 <hiyubin.lee@gmail.com>
2025-01-10 16:39:10 +0900

    feat: Add UserService interface

--- src/main/java/discodeit/service/UserService.java
+++ src/main/java/discodeit/service/UserService.java
@@ -1,4 +1,13 @@
 package discodeit.service;
 
+import discodeit.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    User create(User newUser);
+    User readById(String userId);
+    List<User> readAll();
+    User update(String userId, User updateUser);
+    void delete(String userId);
 }
