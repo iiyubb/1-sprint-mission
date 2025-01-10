2c51454434397b3b777513f5620dbc3192cb26f4
yubin8773 <hiyubin.lee@gmail.com>
2025-01-10 16:41:57 +0900

    feat: Add MessageService interface

--- src/main/java/discodeit/service/MessageService.java
+++ src/main/java/discodeit/service/MessageService.java
@@ -1,4 +1,15 @@
 package discodeit.service;
 
+import discodeit.entity.Channel;
+import discodeit.entity.Message;
+
+import java.util.List;
+
 public interface MessageService {
+    Message create(Message newMessage);
+    Message readById(String messageId);
+    List<Message> readAll();
+    Message updateMessage(String messageId, Message updateMessage);
+    void delete(String messageId);
+    Channel getChannel(String messageId);
 }
