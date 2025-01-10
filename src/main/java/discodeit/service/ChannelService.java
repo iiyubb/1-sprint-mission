762d5c58dcd49df67df7e6ddd09bdbed51853460
yubin8773 <hiyubin.lee@gmail.com>
2025-01-10 16:41:39 +0900

    feat: Add ChannelService interface

--- src/main/java/discodeit/service/ChannelService.java
+++ src/main/java/discodeit/service/ChannelService.java
@@ -1,4 +1,17 @@
 package discodeit.service;
 
+import discodeit.entity.Channel;
+import discodeit.entity.User;
+
+import java.util.List;
+
 public interface ChannelService {
+    Channel create(Channel newChannel);
+    Channel readById(String channelId);
+    List<Channel> readAll();
+    Channel update(String channelId, Channel updateChannel);
+    void deleteChannel(String channelId);
+    void addUser(String channelId, User user);
+    List<User> getUserList(String channelId);
+    void deleteUser(String channelId, User user);
 }
