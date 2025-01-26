4d3ff85d7e84a268de8f81bc4c3c7e29dbfcecf7
yubin8773 <hiyubin.lee@gmail.com>
2025-01-26 20:23:29 +0900

    fix: 채널에 메세지 전송하는 로직으로 수정

--- src/main/java/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/discodeit/service/jcf/JCFMessageService.java
@@ -16,31 +16,7 @@ public class JCFMessageService implements MessageService {
     private Map<String, Message> messageData = new HashMap<>();
 
     @Override
-    public void createDirectMessage(Message newMessage) {
-        String messageId = newMessage.getMessageId();
-        String messageDetail = newMessage.getMessageDetail();
-        User sendUser = newMessage.getSendUser();
-        User receiveUser = newMessage.getReceiveUser();
-
-        // 예외처리
-        if (isMessageIdDuplicate(messageId)) {
-            throw new IllegalArgumentException("[error] 이미 존재하는 메세지 ID입니다.");
-        }
-        if (messageDetail == null || messageDetail.isEmpty()) {
-            throw new IllegalArgumentException("[error] 유효하지 않은 메세지 형식입니다.");
-        }
-        if (sendUser.getUserId() == null || sendUser.getUserId().isEmpty()) {
-            throw new IllegalArgumentException("[error] 존재하지 않는 사용자는 메세지를 전송할 수 없습니다.");
-        }
-        if (receiveUser.getUserId() == null || receiveUser.getUserId().isEmpty()) {
-            throw new IllegalArgumentException("[error] 존재하지 않는 사용자에게 메세지를 전송할 수 없습니다.");
-        }
-
-        messageData.put(messageId, newMessage);
-    }
-
-    @Override
-    public void createGroupMessage(Message newMessage) {
+    public void create(Message newMessage) {
         String messageId = newMessage.getMessageId();
         String messageDetail = newMessage.getMessageDetail();
         User sendUser = newMessage.getSendUser();
