c4526112966716d33e609c525adad516467c71e4
yubin8773 <hiyubin.lee@gmail.com>
2025-01-26 20:14:09 +0900

    feat: use File*Repository

--- src/main/java/discodeit/factory/BasicServiceFactory.java
+++ src/main/java/discodeit/factory/BasicServiceFactory.java
@@ -3,6 +3,9 @@ package discodeit.factory;
 import discodeit.repository.ChannelRepository;
 import discodeit.repository.MessageRepository;
 import discodeit.repository.UserRepository;
+import discodeit.repository.file.FileChannelRepository;
+import discodeit.repository.file.FileMessageRepository;
+import discodeit.repository.file.FileUserRepository;
 import discodeit.repository.jcf.JCFChannelRepository;
 import discodeit.repository.jcf.JCFMessageRepository;
 import discodeit.repository.jcf.JCFUserRepository;
@@ -13,21 +16,27 @@ import discodeit.service.basic.BasicChannelService;
 import discodeit.service.basic.BasicMessageService;
 import discodeit.service.basic.BasicUserService;
 
+import java.nio.file.Path;
+
 
 public class BasicServiceFactory implements ServiceFactory {
 
-    private final UserRepository userRepository;
-    private final ChannelRepository channelRepository;
-    private final MessageRepository messageRepository;
+    private UserRepository userRepository;
+    private ChannelRepository channelRepository;
+    private MessageRepository messageRepository;
 
     private UserService userService;
     private ChannelService channelService;
     private MessageService messageService;
 
     public BasicServiceFactory() {
-        this.userRepository = new JCFUserRepository();
-        this.messageRepository = new JCFMessageRepository();
-        this.channelRepository = new JCFChannelRepository();
+
+    }
+
+    public BasicServiceFactory(Path userPath, Path channelPath, Path messagePath) {
+        this.userRepository = new FileUserRepository(userPath);
+        this.messageRepository = new FileMessageRepository(messagePath);
+        this.channelRepository = new FileChannelRepository(channelPath);
 
         this.userService = new BasicUserService(userRepository);
         this.messageService = new BasicMessageService(messageRepository);
