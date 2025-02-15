f5d3ec8ae461c1225bc339fdfd8aa8b5b6bd0ad4
yubin8773 <hiyubin.lee@gmail.com>
2025-02-15 21:43:29 +0900

    refactor: use refactored method

--- src/main/java/JavaApplication.java
+++ src/main/java/JavaApplication.java
@@ -1,4 +1,5 @@
 import discodeit.entity.Channel;
+import discodeit.entity.ChannelType;
 import discodeit.entity.Message;
 import discodeit.entity.User;
 import discodeit.factory.BasicServiceFactory;
@@ -12,21 +13,16 @@ import java.nio.file.Path;
 
 public class JavaApplication {
     static User setupUser(UserService userService) {
-        User userEntity = new User("woody", "woody@codeit.com", "010-1111-1111");
-        userService.create(userEntity);
-        return userService.readById(userEntity.getUserId());
+        return userService.create("woody", "woody@codeit.com", "010-1111-1111", "woody1234");
     }
 
     static Channel setupChannel(ChannelService channelService) {
-        Channel channelEntity = new Channel("공지");
-        channelService.create(channelEntity);
-        return channelService.readById(channelEntity.getChannelId());
+        return channelService.create("공지", ChannelType.PUBLIC, "공지 채널입니다.");
     }
 
     static void messageCreateTest(MessageService messageService, Channel channel, User author, String messageDetail) {
-        Message messageEntity = new Message(author, channel, messageDetail);
-        messageService.create(messageEntity);
-        System.out.println("메시지 생성: " + messageEntity.getMessageId());
+        Message message = messageService.create(author, channel, messageDetail);
+        System.out.println("메시지 생성: " + message.getMessageDetail());
     }
 
     public static void main(String[] args) {
