7644cf8f46316732a12dcc01df59312f5306eafc
yubin8773 <hiyubin.lee@gmail.com>
2025-02-16 20:04:39 +0900

    feat: check unread message

---
+++ src/main/java/discodeit/entity/ReadStatus.java
@@ -0,0 +1,44 @@
+package discodeit.entity;
+
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+public class ReadStatus {
+    private UUID id;
+    private Instant createdAt;
+
+    private UUID userId;
+    private UUID channelId;
+    private Instant updatedAt;
+    private Instant lastReadAt;
+
+    // 생성자
+    protected ReadStatus() {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+    }
+
+    public ReadStatus(UUID userId, UUID channelId, Instant lastReadAt) {
+        this();
+        this.userId = userId;
+        this.channelId = channelId;
+        this.lastReadAt = lastReadAt;
+    }
+
+    // Setter
+    public void update(Instant newLastReadAt) {
+        if (newLastReadAt != null && !newLastReadAt.equals(this.lastReadAt)) {
+            this.lastReadAt = newLastReadAt;
+            this.updatedAt = Instant.now();
+        }
+    }
+
+    public boolean isOnline() {
+        return lastReadAt.isAfter(Instant.now().minus(Duration.ofMinutes(5)));
+    }
+
+}
