087cc563d1b77abd2957d1b3b4553410975c1dc2
yubin8773 <hiyubin.lee@gmail.com>
2025-02-17 22:44:52 +0900

    fix: add findAllByChannelId method and deleteByChannelId method

--- src/main/java/discodeit/repository/MessageRepository.java
+++ src/main/java/discodeit/repository/MessageRepository.java
@@ -13,6 +13,8 @@ public interface MessageRepository {
     Message save(Message message);
     Optional<Message> findById(UUID id);
     List<Message> findAll();
+    List<Message> findAllByChannelId(UUID channelId);
     boolean existsById(UUID id);
     void deleteById(UUID id);
+    void deleteByChannelId(UUID channelId);
 }
