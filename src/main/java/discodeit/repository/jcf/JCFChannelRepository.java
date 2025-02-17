115fa62fb1a0b4c11d4ff4617cdf2fbba9dd7b75
yubin8773 <hiyubin.lee@gmail.com>
2025-02-17 22:42:07 +0900

    fix: Comment out unused files in jcf

--- src/main/java/discodeit/repository/jcf/JCFChannelRepository.java
+++ src/main/java/discodeit/repository/jcf/JCFChannelRepository.java
@@ -1,43 +1,43 @@
-package discodeit.repository.jcf;
-
-import discodeit.entity.Channel;
-import discodeit.repository.ChannelRepository;
-import discodeit.utils.FileUtil;
-
-import java.util.*;
-
-public class JCFChannelRepository implements ChannelRepository {
-    Map<String, Channel> channelData;
-
-    public JCFChannelRepository() {
-        this.channelData = new HashMap<>();
-    }
-
-    public Channel save(Channel channel) {
-        channelData.put(channel.getId().toString(), channel);
-        return channel;
-    }
-
-    @Override
-    public Optional<Channel> findById(UUID channelId) {
-        if (!channelData.containsKey(channelId.toString())) {
-            throw new IllegalArgumentException("[error] 존재하지 않는 channel ID입니다.");
-        }
-        return Optional.ofNullable(channelData.get(channelId.toString()));
-    }
-
-    @Override
-    public List<Channel> findAll() {
-        return channelData.values().stream().toList();
-    }
-
-    @Override
-    public boolean existsById(UUID channelId) {
-        return channelData.containsKey(channelId.toString());
-    }
-
-    @Override
-    public void deleteById(UUID channelId) {
-        channelData.remove(channelId.toString());
-    }
-}
+//package discodeit.repository.jcf;
+//
+//import discodeit.entity.Channel;
+//import discodeit.repository.ChannelRepository;
+//import discodeit.utils.FileUtil;
+//
+//import java.util.*;
+//
+//public class JCFChannelRepository implements ChannelRepository {
+//    Map<String, Channel> channelData;
+//
+//    public JCFChannelRepository() {
+//        this.channelData = new HashMap<>();
+//    }
+//
+//    public Channel save(Channel channel) {
+//        channelData.put(channel.getId().toString(), channel);
+//        return channel;
+//    }
+//
+//    @Override
+//    public Optional<Channel> findById(UUID channelId) {
+//        if (!channelData.containsKey(channelId.toString())) {
+//            throw new IllegalArgumentException("[error] 존재하지 않는 channel ID입니다.");
+//        }
+//        return Optional.ofNullable(channelData.get(channelId.toString()));
+//    }
+//
+//    @Override
+//    public List<Channel> findAll() {
+//        return channelData.values().stream().toList();
+//    }
+//
+//    @Override
+//    public boolean existsById(UUID channelId) {
+//        return channelData.containsKey(channelId.toString());
+//    }
+//
+//    @Override
+//    public void deleteById(UUID channelId) {
+//        channelData.remove(channelId.toString());
+//    }
+//}
--- src/main/java/discodeit/repository/jcf/JCFUserRepository.java
+++ src/main/java/discodeit/repository/jcf/JCFUserRepository.java
@@ -1,45 +1,45 @@
-package discodeit.repository.jcf;
-
-import discodeit.entity.User;
-import discodeit.repository.UserRepository;
-import discodeit.utils.FileUtil;
-
-import java.util.*;
-
-public class JCFUserRepository implements UserRepository {
-    private Map<String, User> userData;
-
-    public JCFUserRepository() {
-        this.userData = new HashMap<>();
-    }
-
-    @Override
-    public User save(User user) {
-        userData.put(user.getId().toString(), user);
-        return user;
-    }
-
-    @Override
-    public Optional<User> findById(UUID userId) {
-        if (!userData.containsKey(userId.toString())) {
-            throw new IllegalArgumentException("[error] 존재하지 않는 user ID입니다.");
-        }
-        return Optional.ofNullable(userData.get(userId.toString()));
-    }
-
-    @Override
-    public List<User> findAll() {
-        return userData != null ? userData.values().stream().toList() : new ArrayList<>();
-    }
-
-    @Override
-    public boolean existsById(UUID userId) {
-        return userData.containsKey(userId.toString());
-    }
-
-    @Override
-    public void deleteById(UUID userId) {
-        userData.remove(userId.toString());
-    }
-
-}
+//package discodeit.repository.jcf;
+//
+//import discodeit.entity.User;
+//import discodeit.repository.UserRepository;
+//import discodeit.utils.FileUtil;
+//
+//import java.util.*;
+//
+//public class JCFUserRepository implements UserRepository {
+//    private Map<String, User> userData;
+//
+//    public JCFUserRepository() {
+//        this.userData = new HashMap<>();
+//    }
+//
+//    @Override
+//    public User save(User user) {
+//        userData.put(user.getId().toString(), user);
+//        return user;
+//    }
+//
+//    @Override
+//    public Optional<User> findById(UUID userId) {
+//        if (!userData.containsKey(userId.toString())) {
+//            throw new IllegalArgumentException("[error] 존재하지 않는 user ID입니다.");
+//        }
+//        return Optional.ofNullable(userData.get(userId.toString()));
+//    }
+//
+//    @Override
+//    public List<User> findAll() {
+//        return userData != null ? userData.values().stream().toList() : new ArrayList<>();
+//    }
+//
+//    @Override
+//    public boolean existsById(UUID userId) {
+//        return userData.containsKey(userId.toString());
+//    }
+//
+//    @Override
+//    public void deleteById(UUID userId) {
+//        userData.remove(userId.toString());
+//    }
+//
+//}
