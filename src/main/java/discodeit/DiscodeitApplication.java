4b10a47e2bbd52cb831711fb9e3e7d9974ee5738
yubin8773 <hiyubin.lee@gmail.com>
2025-02-17 22:35:15 +0900

    test: SpringBootApplication

--- src/main/java/discodeit/DiscodeitApplication.java
+++ src/main/java/discodeit/DiscodeitApplication.java
@@ -1,7 +1,9 @@
 package discodeit;
 
+import discodeit.dto.channel.CreatePublicChannelRequest;
+import discodeit.dto.message.CreateMessageRequest;
+import discodeit.dto.user.CreateUserRequest;
 import discodeit.entity.Channel;
-import discodeit.entity.ChannelType;
 import discodeit.entity.Message;
 import discodeit.entity.User;
 import discodeit.service.ChannelService;
@@ -11,42 +13,46 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @SpringBootApplication
 public class DiscodeitApplication {
-
     static User setupUser(UserService userService) {
-        return userService.create("woody", "woody@codeit.com", "010-1111-1111", "woody1234");
+        CreateUserRequest request = new CreateUserRequest("woody", "woody@codeit.com", "010-1111-2222", "woody1234");
+        User user = userService.create(request, Optional.empty());
+        return user;
     }
 
     static Channel setupChannel(ChannelService channelService) {
-        return channelService.create("공지", ChannelType.PUBLIC, "공지 채널입니다.");
+        CreatePublicChannelRequest request = new CreatePublicChannelRequest("공지", "공지 채널입니다.", Optional.empty());
+        Channel channel = channelService.create(request);
+        return channel;
     }
 
-    static void messageCreateTest(MessageService messageService, Channel channel, User author, String messageDetail) {
-        Message message = messageService.create(author, channel, messageDetail);
-        System.out.println("메시지 생성: " + message.getMessageDetail());
+    static void messageCreateTest(MessageService messageService, Channel channel, User author) {
+        CreateMessageRequest request = new CreateMessageRequest("안녕하세요.", channel.getId(), author.getId());
+        Message message = messageService.create(request, new ArrayList<>());
+        System.out.println("메시지 생성: " + message.getId());
     }
 
     private static UserService userService;
     private static ChannelService channelService;
     private static MessageService messageService;
 
+
     public static void main(String[] args) {
 
         ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
 
         // 서비스 초기화
-        // TODO context에서 Bean을 조회하여 각 서비스 구현체 할당 코드 작성하세요.
-        // TODO: ??? 여기서 파라미터 주려면 어떻게 해야하는거지?
         userService = context.getBean(UserService.class);
         channelService = context.getBean(ChannelService.class);
         messageService = context.getBean(MessageService.class);
 
         User user = setupUser(userService);
         Channel channel = setupChannel(channelService);
-        messageCreateTest(messageService, channel, user, "Test!!!!!!!!");
+        messageCreateTest(messageService, channel, user);
     }
 
 }
