68994ffdb4207cd80cf75c24e1fa342eeaa23425
yubin8773 <hiyubin.lee@gmail.com>
2025-02-17 22:46:46 +0900

    fix: add findByUsername method

--- src/main/java/discodeit/repository/UserRepository.java
+++ src/main/java/discodeit/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package discodeit.repository                                                                                                                                                                                                                                                                                                                                                                                                                   ;
 
+import discodeit.dto.user.UserDto;
 import discodeit.entity.User;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.UUID;
 public interface UserRepository {
     User save(User user);
     Optional<User> findById(UUID id);
+    Optional<User> findByUsername(String username);
     List<User> findAll();
     boolean existsById(UUID id);
     void deleteById(UUID id);
