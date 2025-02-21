a65ee817c7734b6e2aeb85357d67c8f4b8c37927
yubin8773 <hiyubin.lee@gmail.com>
2025-02-21 13:03:00 +0900

    refactor: add fildAllByIdIn method

--- src/main/java/discodeit/repository/BinaryContentRepository.java
+++ src/main/java/discodeit/repository/BinaryContentRepository.java
@@ -2,12 +2,14 @@ package discodeit.repository;
 
 import discodeit.entity.BinaryContent;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BinaryContentRepository {
     BinaryContent save(BinaryContent binaryContent);
     Optional<BinaryContent> findById(UUID id);
+    List<BinaryContent> findAllByIdIn(List<UUID> binaryContentIds);
     boolean existsById(UUID id);
     void deleteById(UUID id);
 }
