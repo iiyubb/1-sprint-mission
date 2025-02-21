54cd5015c643cc3ab6e1da28dee5d895bb3b83b3
yubin8773 <hiyubin.lee@gmail.com>
2025-02-21 13:05:53 +0900

    refactor: add findAllbyUserId method

--- src/main/java/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/discodeit/repository/ReadStatusRepository.java
@@ -2,12 +2,14 @@ package discodeit.repository;
 
 import discodeit.entity.ReadStatus;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ReadStatusRepository {
     ReadStatus save(ReadStatus readStatus);
     Optional<ReadStatus> findById(UUID id);
+    List<ReadStatus> findAllByUserId(UUID userId);
     boolean existsById(UUID id);
     void deleteById(UUID id);
     void deleteByChannelId(UUID channelId);
