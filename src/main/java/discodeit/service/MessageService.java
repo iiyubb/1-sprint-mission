f117b380e356c5e3c197572fca3b3e4619b1e74b
yubin8773 <hiyubin.lee@gmail.com>
2025-02-23 23:55:11 +0900

    fix: use entity instead of DTO

--- src/main/java/discodeit/service/MessageService.java
+++ src/main/java/discodeit/service/MessageService.java
@@ -1,8 +1,6 @@
 package discodeit.service;
 
-import discodeit.dto.binarycontent.AddBinaryContentRequest;
 import discodeit.dto.message.CreateMessageRequest;
-import discodeit.dto.message.MessageDto;
 import discodeit.dto.message.UpdateMessageRequest;
 import discodeit.entity.Message;
 
@@ -10,9 +8,9 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
-    Message create(CreateMessageRequest createMessageRequest, List<AddBinaryContentRequest> addBinaryContentRequests);
-    MessageDto find(UUID messageId);
-    List<MessageDto> findAllByChannelId(UUID channelId);
-    Message update(UUID messageId, UpdateMessageRequest updateMessageRequest);
-    void delete(UUID messageId, UUID writerId);
+    Message create(CreateMessageRequest createMessageRequest);
+    Message find(UUID messageId);
+    List<Message> findAllByChannelId(UUID channelId);
+    Message update(UpdateMessageRequest updateMessageRequest);
+    void delete(UUID messageId);
 }
