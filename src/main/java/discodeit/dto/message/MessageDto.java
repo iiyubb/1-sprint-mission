4a7e24387c865e6c3a229f9e5e93204cd7b447a1
yubin8773 <hiyubin.lee@gmail.com>
2025-02-23 23:50:18 +0900

    feat: add domain to DTO method

--- src/main/java/discodeit/dto/message/MessageDto.java
+++ src/main/java/discodeit/dto/message/MessageDto.java
@@ -1,12 +1,15 @@
 package discodeit.dto.message;
 
 import discodeit.entity.BinaryContent;
+import discodeit.entity.Message;
+import lombok.Builder;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Builder
 public record MessageDto(UUID id,
                          Instant createdAt,
                          String messageDetail,
@@ -14,4 +17,15 @@ public record MessageDto(UUID id,
                          UUID channelId,
                          List<UUID> attachedIds,
                          Instant updatedAt) {
+
+    public static MessageDto fromDomain(Message message) {
+        return MessageDto.builder()
+                .id(message.getId())
+                .messageDetail(message.getMessageDetail())
+                .sendUserId(message.getSendUserId())
+                .channelId(message.getChannelId())
+                .attachedIds(message.getAttachmentIds())
+                .updatedAt(message.getUpdatedAt())
+                .build();
+    }
 }
