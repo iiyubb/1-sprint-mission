f5dbe2572a360b487b74d08a8f21ba7d9c24cc84
yubin8773 <hiyubin.lee@gmail.com>
2025-02-23 23:49:25 +0900

    fix: use entity instead of DTO

--- src/main/java/discodeit/service/ChannelService.java
+++ src/main/java/discodeit/service/ChannelService.java
@@ -1,13 +1,9 @@
 package discodeit.service;
 
-import discodeit.dto.channel.ChannelDto;
 import discodeit.dto.channel.CreatePrivateChannelRequest;
 import discodeit.dto.channel.CreatePublicChannelRequest;
 import discodeit.dto.channel.UpdateChannelRequest;
 import discodeit.entity.Channel;
-import discodeit.entity.ChannelType;
-import discodeit.entity.Message;
-import discodeit.entity.User;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,9 +11,9 @@ import java.util.UUID;
 public interface ChannelService {
     Channel create(CreatePublicChannelRequest createPublicChannelRequest);
     Channel create(CreatePrivateChannelRequest createPrivateChannelRequest);
-    ChannelDto find(UUID channelId);
-    List<ChannelDto> findAllByUserId(UUID userId);
-    Channel update(UUID channelId, UpdateChannelRequest updateChannelRequest);
+    Channel find(UUID channelId);
+    List<Channel> findAllByUserId(UUID userId);
+    Channel update(UpdateChannelRequest updateChannelRequest);
     void delete(UUID channelId);
     void addParticipant(UUID channelId, UUID userId);
     void deleteParticipant(UUID channelId, UUID userId);
