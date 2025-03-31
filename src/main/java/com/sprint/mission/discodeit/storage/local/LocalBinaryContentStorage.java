8a27de6f48522ed5105019970613aafb7e57780c
iiyubb <hiyubin.lee@gmail.com>
2025-03-31 13:35:17 +0900

    feat: Add logging in service

--- src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/local/LocalBinaryContentStorage.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.InputStreamResource;
@@ -19,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @ConditionalOnProperty(name = "discodeit.storage.type", havingValue = "local")
 @Component
 public class LocalBinaryContentStorage implements BinaryContentStorage {
@@ -33,11 +35,14 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
 
   @PostConstruct
   public void init() {
+    log.info("[파일 초기화 시도]");
+
     if (!Files.exists(root)) {
       try {
+        log.info("[파일 저장 경로 생성 시도]");
         Files.createDirectories(root);
       } catch (IOException e) {
-        e.printStackTrace();
+        log.error("[파일 저장 경로 생성 실패]", e);
         throw new RuntimeException(e);
       }
     }
@@ -45,12 +50,18 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
 
   public UUID put(UUID binaryContentId, byte[] bytes) {
     Path filePath = resolvePath(binaryContentId);
+    log.info("[파일 저장 시도] 파일 ID: {}", binaryContentId);
+
     if (Files.exists(filePath)) {
+      log.error("[파일 저장 실패] 해당 파일이 이미 존재합니다. 파일 ID: {}", binaryContentId);
       throw new IllegalArgumentException("File with key " + binaryContentId + " already exists");
     }
+
     try (OutputStream outputStream = Files.newOutputStream(filePath)) {
       outputStream.write(bytes);
+      log.info("[파일 저장 성공] 파일 ID: {}", binaryContentId);
     } catch (IOException e) {
+      log.error("[파일 저장 실패] 파일 ID: {}}", binaryContentId, e);
       throw new RuntimeException(e);
     }
     return binaryContentId;
@@ -58,13 +69,17 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
 
   public InputStream get(UUID binaryContentId) {
     Path filePath = resolvePath(binaryContentId);
+    log.info("[파일 변환 시도] 파일 ID: {}", binaryContentId);
+
     if (Files.notExists(filePath)) {
+      log.error("[파일 조회 실패] 해당 파일을 찾을 수 없습니다. 파일 ID: {}", binaryContentId);
       throw new NoSuchElementException("File with key " + binaryContentId + " does not exist");
     }
     try {
+      log.info("[파일 변환 성공] 파일 ID: {}", binaryContentId);
       return Files.newInputStream(filePath);
     } catch (IOException e) {
-      e.printStackTrace();
+      log.error("[파일 변환 실패] 파일을 InputStream 타입으로 변환 실패했습니다. 파일 ID: {}", binaryContentId, e);
       throw new RuntimeException(e);
     }
   }
@@ -77,6 +92,7 @@ public class LocalBinaryContentStorage implements BinaryContentStorage {
   public ResponseEntity<Resource> download(BinaryContentDto metaData) {
     InputStream inputStream = get(metaData.id());
     Resource resource = new InputStreamResource(inputStream);
+    log.info("[파일 다운로드 시도] 파일 ID: {}", metaData.id());
 
     return ResponseEntity
         .status(HttpStatus.OK)
